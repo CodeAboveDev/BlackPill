8b60f1b143a965769827718726987c60cf894951
Luuke <lukasz.bartosz1990@gmail.com>
2020-12-11 21:14:38 +0100

    Added Gpio class definition

--- Source/Mcu/Gpio.h
+++ Source/Mcu/Gpio.h
@@ -11,6 +11,21 @@
 #ifndef Gpio_H
 #define Gpio_H
 
+#include <stdint.h>
 
+class Gpio
+{
+private:
+    volatile uint32_t MODER;
+    volatile uint32_t OTYPER;
+    volatile uint32_t OSPEEDR;
+    volatile uint32_t PUPDR;
+    volatile uint32_t IDR;
+    volatile uint32_t ODR;
+    volatile uint32_t BSRR;
+    volatile uint32_t LCKR;
+    volatile uint32_t AFRL;
+    volatile uint32_t AFRH;    
+};
 
 #endif // Gpio_H
