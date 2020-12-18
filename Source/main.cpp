4e07e2803c93eb831206dd260161b8215c710080
Luuke <lukasz.bartosz1990@gmail.com>
2020-12-18 10:43:37 +0100

    Controlling blue LED with user switch

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,17 +1,34 @@
 #include <stdint.h>
 #include "Mcu/Gpio.h"
+#include "Led.h"
+#include "Switch.h"
 #include "stm32f411xe.h"
 
 int main(void)
 {
     Gpio &gpioC = *reinterpret_cast<Gpio *>(GPIOC_BASE);
+    Gpio &gpioA = *reinterpret_cast<Gpio *>(GPIOA_BASE);
+    GpioPin pinC13 {gpioC, Gpio::Pin::Pin_13};
+    GpioPin pinA0 {gpioA, Gpio::Pin::Pin_0};
 
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 
-    gpioC.SetPinMode(Gpio::Pin::Pin_13, Gpio::Mode::Output);
+    Led blueLed(pinC13);
+    Switch userSwitch(pinA0);
+    blueLed.On();
+    blueLed.Off();
 
     while(1)
     {
-
+        if(userSwitch.GetState() == true)
+        {
+            blueLed.On();
+        }
+        else
+        {
+            blueLed.Off();
+        }
+        
     }
 }
