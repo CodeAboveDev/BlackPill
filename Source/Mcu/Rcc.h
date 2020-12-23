fcd7e89783595008d9522c05884cd610235a7532
Luuke <lukasz.bartosz1990@gmail.com>
2020-12-23 13:10:51 +0100

    Added registers

--- Source/Mcu/Rcc.h
+++ Source/Mcu/Rcc.h
@@ -11,14 +11,51 @@
 #ifndef Rcc_H
 #define Rcc_H
 
+#include <stdint.h>
+#include <type_traits>
+
 class Rcc
 {
 public:
-    
+
 
 private:
-
-
+    uint32_t CR;
+    uint32_t PLLCFGR;
+    uint32_t CFGR;
+    uint32_t CIR;
+    uint32_t AHB1RSTR;
+    uint32_t AHB2RSTR;
+    uint32_t Reserved1;
+    uint32_t Reserved2;
+    uint32_t APB1RSTR;
+    uint32_t APB2RSTR;
+    uint32_t Reserved3;
+    uint32_t Reserved4;
+    uint32_t AHB1ENR;
+    uint32_t AHB2ENR;
+    uint32_t Reserved5;
+    uint32_t Reserved6;
+    uint32_t APB1ENR;
+    uint32_t APB2ENR;
+    uint32_t Reserved7;
+    uint32_t Reserved8;
+    uint32_t AHB1LPENR;
+    uint32_t AHB2LPENR;
+    uint32_t Reserved;
+    uint32_t Reserved;
+    uint32_t APB1LPENR;
+    uint32_t APB2LPENR; 
+    uint32_t Reserved;
+    uint32_t Reserved;
+    uint32_t BDCR;
+    uint32_t CSR;
+    uint32_t Reserved;
+    uint32_t Reserved;
+    uint32_t SSCGR;
+    uint32_t PLLI2SCFGR;
+    uint32_t Reserved;
+    uint32_t DCKCFGR;
 };
 
 static_assert(std::is_standard_layout<Rcc>::value, "Rcc class is NOT standard layout");
