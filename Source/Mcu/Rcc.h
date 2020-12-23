be6e8d31972c42d79d985e8fb9f9acdfda81dcb7
Luuke <lukasz.bartosz1990@gmail.com>
2020-12-23 12:32:46 +0100

    Created Rcc files with empty class

---
+++ Source/Mcu/Rcc.h
@@ -0,0 +1,26 @@
+/*****************************************************************
+ * Copyright (c) 2020 CodeAbove.
+ * All rights reserved.
+ *
+ * This software component is licensed by CodeAbove under
+ * BSD 3-Clause license, the 'License'. You may not use
+ * this file except in compliance with the License. You
+ * may obtain a copy of the License at:
+ *                  opensource.org/licenses/BSD-3-Claus
+ ****************************************************************/
+#ifndef Rcc_H
+#define Rcc_H
+
+class Rcc
+{
+public:
+    
+
+private:
+
+
+};
+
+static_assert(std::is_standard_layout<Rcc>::value, "Rcc class is NOT standard layout");
+
+#endif // Rcc_H
