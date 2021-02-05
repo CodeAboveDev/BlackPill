888e7619bfdd2fdbbda0bc97a7109fcd30c6e2f6
Luuke <lukasz.bartosz1990@gmail.com>
2021-02-05 16:19:12 +0100

    Added class with registers

--- Source/Mcu/Spi.h
+++ Source/Mcu/Spi.h
@@ -11,6 +11,26 @@
 #ifndef Spi_H
 #define Spi_H
 
+#include <stdint.h>
+#include <type_traits>
 
+class Spi
+{
+public:
+
+
+private:
+    volatile uint32_t CR1;
+    volatile uint32_t SR;
+    volatile uint32_t DR;
+    volatile uint32_t CRCPR;
+    volatile uint32_t RXCRCR;
+    volatile uint32_t TXCRCR;
+    volatile uint32_t I2SCFGR;
+    volatile uint32_t I2SPR;
+
+};
+
+static_assert(std::is_standard_layout<Spi>::value, "SPI class is NOT standard layout");
 
 #endif // Spi_H
