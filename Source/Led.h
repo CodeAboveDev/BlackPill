6d5be59a4a4f6764a88dfff051cdb5afa486580f
Luuke <lukasz.bartosz1990@gmail.com>
2021-02-05 15:36:51 +0100

    Fixed typo in license link

--- Source/Led.h
+++ Source/Led.h
@@ -6,7 +6,7 @@
  * BSD 3-Clause license, the 'License'. You may not use
  * this file except in compliance with the License. You
  * may obtain a copy of the License at:
- *                  opensource.org/licenses/BSD-3-Claus
+ *                  opensource.org/licenses/BSD-3-Clause
  ****************************************************************/
 #ifndef Led_H
 #define Led_H
