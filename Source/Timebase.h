8509b96075a019348c7a578fda8345dd9d528dfb
Luuke <lukasz.bartosz1990@gmail.com>
2021-02-23 14:34:06 +0100

    Created TimeBase files

---
+++ Source/Timebase.h
@@ -0,0 +1,22 @@
+/*****************************************************************
+ * Copyright (c) 2021 CodeAbove.
+ * All rights reserved.
+ *
+ * This software component is licensed by CodeAbove under
+ * BSD 3-Clause license, the 'License'. You may not use
+ * this file except in compliance with the License. You
+ * may obtain a copy of the License at:
+ *                  opensource.org/licenses/BSD-3-Clause
+ ****************************************************************/
+#ifndef TimeBase_H
+#define TimeBase_H
+
+class TimeBase
+{
+public:
+
+private:
+
+};
+
+#endif // TimeBase_H
