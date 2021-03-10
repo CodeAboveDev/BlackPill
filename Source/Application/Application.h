54b498d14f9414f7ecde8eea1624f164c8e1bba2
Luuke <lukasz.bartosz1990@gmail.com>
2021-03-10 16:38:16 +0100

    Created Application class files

---
+++ Source/Application/Application.h
@@ -0,0 +1,25 @@
+/*****************************************************************
+ * Copyright (c) 2021 CodeAbove.
+ * All rights reserved.
+ *
+ * This software component is licensed by CodeAbove under
+ * BSD 3-Clause license, the 'License'. You may not use
+ * this file except in compliance with the License. You
+ * may obtain a copy of the License at:
+ *                  opensource.org/licenses/BSD-3-Clause
+ ****************************************************************/
+#ifndef Application_H
+#define Application_H
+
+class Application
+{
+public:
+    Application();
+    
+
+private:
+
+
+};
+
+#endif // Application_H
