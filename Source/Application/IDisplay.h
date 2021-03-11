3f0170ba00f72e3654d53916d7a9774e6a6a1a8f
Luuke <lukasz.bartosz1990@gmail.com>
2021-03-11 18:16:07 +0100

    Created display interface class file

---
+++ Source/Application/IDisplay.h
@@ -0,0 +1,24 @@
+/*****************************************************************
+ * Copyright (c) 2021 CodeAbove.
+ * All rights reserved.
+ *
+ * This software component is licensed by CodeAbove under
+ * BSD 3-Clause license, the 'License'. You may not use
+ * this file except in compliance with the License. You
+ * may obtain a copy of the License at:
+ *                  opensource.org/licenses/BSD-3-Clause
+ ****************************************************************/
+#ifndef IDisplay_H
+#define IDisplay_H
+
+class IDisplay
+{
+public:
+    
+
+private:
+
+
+};
+
+#endif // IDisplay_H
