1176444625709d008a3f0d9282a96f8c58aad25a
Luuke <lukasz.bartosz1990@gmail.com>
2021-05-06 15:11:29 +0200

    Created ST7789Display class as Display implementation

---
+++ Source/Application/Display/_impl/ST7789Display.cpp
@@ -0,0 +1,23 @@
+/*****************************************************************
+ * Copyright (c) 2021 CodeAbove.
+ * All rights reserved.
+ *
+ * This software component is licensed by CodeAbove under
+ * BSD 3-Clause license, the 'License'. You may not use
+ * this file except in compliance with the License. You
+ * may obtain a copy of the License at:
+ *                  opensource.org/licenses/BSD-3-Clause
+ ****************************************************************/
+#include "ST7789Display.h"
+#include "../../../Drivers/ST7789/ST7789.h"
+
+ST7789Display::ST7789Display(uint16_t x, uint16_t y, IST7789Spi& spi, IST7789Pin& rstPin, IST7789Pin& dcPin, uint8_t* buf)
+: IDisplay(x,y), st7789(spi, rstPin, dcPin, buf)
+{
+    st7789.Init();
+}
+
+void ST7789Display::Refresh(void)
+{
+    st7789.RefreshDisplay();
+}
---
+++ Source/Application/Display/_impl/ST7789Display.h
@@ -0,0 +1,30 @@
+/*****************************************************************
+ * Copyright (c) 2021 CodeAbove.
+ * All rights reserved.
+ *
+ * This software component is licensed by CodeAbove under
+ * BSD 3-Clause license, the 'License'. You may not use
+ * this file except in compliance with the License. You
+ * may obtain a copy of the License at:
+ *                  opensource.org/licenses/BSD-3-Clause
+ ****************************************************************/
+#ifndef ST7789Display_H
+#define ST7789Display_H
+
+#include "../IDisplay.h"
+#include "../../../Drivers/ST7789/ST7789.h"
+
+class ST7789Display : public IDisplay
+{
+public:
+    ST7789Display(uint16_t x, uint16_t y, IST7789Spi& spi, IST7789Pin& rstPin, IST7789Pin& dcPin, uint8_t* buf);
+    
+    void Refresh(void) final override;
+
+
+private:
+    ST7789 st7789;
+
+};
+
+#endif // ST7789Display_H
