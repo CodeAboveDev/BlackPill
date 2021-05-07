25cdab0991d1201a6a13f412e389740022fff45a
Luuke <lukasz.bartosz1990@gmail.com>
2021-05-07 18:55:33 +0200

    Created class for PCA9685 controlled servo

---
+++ Source/Application/Servo/_impl/PCA9685Servo.h
@@ -0,0 +1,29 @@
+/*****************************************************************
+ * Copyright (c) 2021 CodeAbove.
+ * All rights reserved.
+ *
+ * This software component is licensed by CodeAbove under
+ * BSD 3-Clause license, the 'License'. You may not use
+ * this file except in compliance with the License. You
+ * may obtain a copy of the License at:
+ *                  opensource.org/licenses/BSD-3-Clause
+ ****************************************************************/
+#ifndef PCA9685Servo_H
+#define PCA9685Servo_H
+
+namespace CodeAbove
+{
+
+class PCA9685Servo
+{
+public:
+
+
+private:
+    
+
+};
+
+} // namespace CodeAbove
+
+#endif // PCA9685Servo_H
