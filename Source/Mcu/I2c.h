f40ea6ddc972e95cf71386985dd2a393859fd3b7
Luuke <lukasz.bartosz1990@gmail.com>
2021-05-12 18:13:30 +0200

    Created I2C files with empty class

---
+++ Source/Mcu/I2c.h
@@ -0,0 +1,24 @@
+/*****************************************************************
+ * Copyright (c) 2021 CodeAbove.
+ * All rights reserved.
+ *
+ * This software component is licensed by CodeAbove under
+ * BSD 3-Clause license, the 'License'. You may not use
+ * this file except in compliance with the License. You
+ * may obtain a copy of the License at:
+ *                  opensource.org/licenses/BSD-3-Clause
+ ****************************************************************/
+#ifndef I2c_H
+#define I2c_H
+
+class I2c
+{
+public:
+
+
+private:
+
+
+};
+
+#endif // I2c_H
