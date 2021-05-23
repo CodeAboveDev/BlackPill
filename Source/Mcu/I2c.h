4bab2d1444f759f9f34ca317a8f6bb6502d96098
Luuke <lukasz.bartosz1990@gmail.com>
2021-05-23 19:44:47 +0200

    Defined unions for registers

--- Source/Mcu/I2c.h
+++ Source/Mcu/I2c.h
@@ -17,22 +17,80 @@
 class I2c
 {
 public:
+    enum class ClockSpeedMode : uint8_t
+    {
+        Standard,
+        Fast
+    };
 
+    enum class DutyCycle : uint8_t
+    {
+        DC_2,
+        DC_16_9,
+    };
 
     void SetClockSped(uint32_t speed, uint32_t pclk1ClockFrequency);
     void SetOwnAddress1(uint8_t address);
     void Enable(void);
 
 private:
+    static constexpr uint32_t StandardSpeedMax_kHz { 100000 }; 
+    static constexpr uint32_t FastSpeedMax_kHz { 400000 };
+
+    union CR2_register
+    {
+        struct CR2_bits
+        {
+            uint8_t FREQ : 6;
+            const uint8_t Reserved1 : 2;
+            uint8_t ITERREN : 1;
+            uint8_t ITEVTEN : 1;
+            uint8_t ITBUFEN : 1;
+            uint8_t DMAEN : 1;
+            uint8_t LAST : 1;
+            uint32_t Reserved2 : 19;
+        } __attribute__((packed));
+
+        uint32_t value;
+        struct CR2_bits bits;
+    };
+
+    union CCR_register
+    {
+        struct CCR_bits
+        {
+            uint16_t CCR : 12;
+            const uint8_t Reserved1 : 2;
+            DutyCycle DUTY : 1;
+            ClockSpeedMode FS : 1;
+            const uint16_t Reserved2 : 16;
+        } __attribute__((packed));
+
+        uint32_t value;
+        struct CCR_bits bits;
+    };
+
+    union TRISE_register
+    {
+        struct TRISE_bits
+        {
+            uint8_t TRISE : 6;
+            const uint32_t Reserved : 26;
+        } __attribute__((packed));
+
+        uint32_t value;
+        struct TRISE_bits bits;
+    };
+
     volatile uint32_t CR1;
-    volatile uint32_t CR2;
+    volatile union CR2_register CR2;
     volatile uint32_t OAR1;
     volatile uint32_t OAR2;
     volatile uint32_t DR;
     volatile uint32_t SR1;
     volatile uint32_t SR2;
-    volatile uint32_t CCR;
-    volatile uint32_t TRISE;
+    volatile union CCR_register CCR;
+    volatile union TRISE_register TRISE;
     volatile uint32_t FLTR;
 
 };
